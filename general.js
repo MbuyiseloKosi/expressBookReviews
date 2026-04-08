efcfd8baa0e62bacdec53f310b966ec3fc4511b4
Mbuyiselo Kosi <mbuyi.kosi@gmail.com>
2026-04-08 14:14:52 +0200

    Add general.js for Task 11

--- general.js
+++ general.js
@@ -4,45 +4,29 @@ const BASE_URL = "http://localhost:3000";
 
 // Get all books
 async function getAllBooks() {
-    try {
-        const res = await axios.get(`${BASE_URL}/books`);
-        console.log(res.data);
-    } catch (err) {
-        console.error(err.message);
-    }
+  const res = await axios.get(`${BASE_URL}/books`);
+  console.log(res.data);
 }
 
 // Get by ISBN
 async function getByISBN(isbn) {
-    try {
-        const res = await axios.get(`${BASE_URL}/books/isbn/${isbn}`);
-        console.log(res.data);
-    } catch (err) {
-        console.error(err.message);
-    }
+  const res = await axios.get(`${BASE_URL}/books/isbn/${isbn}`);
+  console.log(res.data);
 }
 
 // Get by Author
 async function getByAuthor(author) {
-    try {
-        const res = await axios.get(`${BASE_URL}/books/author/${author}`);
-        console.log(res.data);
-    } catch (err) {
-        console.error(err.message);
-    }
+  const res = await axios.get(`${BASE_URL}/books/author/${author}`);
+  console.log(res.data);
 }
 
 // Get by Title
 async function getByTitle(title) {
-    try {
-        const res = await axios.get(`${BASE_URL}/books/title/${title}`);
-        console.log(res.data);
-    } catch (err) {
-        console.error(err.message);
-    }
+  const res = await axios.get(`${BASE_URL}/books/title/${title}`);
+  console.log(res.data);
 }
 
-// Example calls (you can comment/uncomment)
+// Run tests
 getAllBooks();
 getByISBN("111");
 getByAuthor("John");
